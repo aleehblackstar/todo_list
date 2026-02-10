8880741c5d9d0b420d497fb3639744db9dd16ff3
Alexandre Rodrigues <aleeh.rodrigues92@gmail.com>
2026-02-10 00:08:08 -0300

    feat: adicionando cards tasks

--- lib/pages/tasks_list.page.dart
+++ lib/pages/tasks_list.page.dart
@@ -7,6 +7,34 @@ class TasksListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Tarefas'), centerTitle: true),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsetsGeometry.only(top: 5),
+              child: ListTile(
+                title: Text("Marcar uma reunião"),
+                subtitle: Text("Uma reunião para discutir o projeto"),
+                leading: Checkbox(value: false, onChanged: (value) {}),
+                trailing: Icon(Icons.star),
+                tileColor: Colors.indigo[50],
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsetsGeometry.only(top: 10),
+              child: ListTile(
+                title: Text("Marcar uma reunião"),
+                subtitle: Text("Uma reunião para discutir o projeto"),
+                leading: Checkbox(value: false, onChanged: (value) {}),
+                trailing: Icon(Icons.star),
+                tileColor: Colors.indigo[50],
+              ),
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {},
         label: const Text('Nova Tarefa'),
