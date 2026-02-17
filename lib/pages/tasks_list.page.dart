ce7f4feebde1dd54718c5bb2c31e9719f79385fc
Alexandre Rodrigues <aleeh.rodrigues92@gmail.com>
2026-02-17 11:03:09 -0300

    feat: modificando detalhes de mudanças de estado nas tarefas

--- lib/pages/tasks_list.page.dart
+++ lib/pages/tasks_list.page.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/models/task.model.dart';
 
-class TasksListPage extends StatelessWidget {
+class TasksListPage extends StatefulWidget {
   const TasksListPage({super.key});
+
+  @override
+  State<TasksListPage> createState() => _TasksListPageState();
+}
+
+class _TasksListPageState extends State<TasksListPage> {
+  final List<Task> tasks = [
+    Task(title: "Estudar", description: "Aula de Flutter", completed: true),
+    Task(
+      title: "Ir na academia",
+      description: "Fazer cardio",
+      important: true,
+      completed: false,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -10,48 +26,46 @@ class TasksListPage extends StatelessWidget {
         title: const Text('Tarefas'),
         centerTitle: true,
         backgroundColor: Colors.indigo,
-        titleTextStyle: TextStyle(
+        titleTextStyle: const TextStyle(
           color: Colors.white,
           fontSize: 20,
           fontWeight: FontWeight.bold,
         ),
       ),
       body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Card(
-              elevation: 3,
-              color: Colors.indigo[50],
-              child: ListTile(
-                title: Text("Marcar uma reunião"),
-                subtitle: Text("Uma reunião para discutir o projeto"),
-                leading: Checkbox(value: false, onChanged: (value) {}),
-                trailing: Icon(Icons.star, color: Colors.indigo),
-                onTap: () {},
-              ),
-            ),
+        padding: const EdgeInsets.all(10),
+        child: ListView.builder(
+          itemCount: tasks.length,
+          itemBuilder: (ctx, index) {
+            final task = tasks[index];
 
-            Card(
+            return Card(
               elevation: 3,
               color: Colors.indigo[50],
               child: ListTile(
-                title: Text("Marcar uma reunião"),
-                subtitle: Text("Uma reunião para discutir o projeto"),
-                leading: Checkbox(value: false, onChanged: (value) {}),
+                title: Text(task.title),
+                subtitle: task.description != null
+                    ? Text(task.description!)
+                    : null,
+                leading: Checkbox(value: task.completed, onChanged: (value) {
+                  setState(() {
+                    task.completed = value ?? false;
+                  });
+                }),
                 trailing: Icon(
-                  Icons.star),
-                  iconColor: Colors.indigo,
+                  task.important ? Icons.star : Icons.star_border,
+                  color: Colors.indigo,
+                ),
                 onTap: () {},
               ),
-            ),
-          ],
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {},
         label: const Text('Nova Tarefa'),
-        icon: const Icon(Icons.person_add),
+        icon: const Icon(Icons.add),
       ),
     );
   }
