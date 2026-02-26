b0b418b79424b00044730a6a608da74b0606ea6f
Alexandre Rodrigues <aleeh.rodrigues92@gmail.com>
2026-02-25 22:30:09 -0300

    style: criando row de data de criação da tarefa e icone de deletar

--- lib/pages/task_detail.page.dart
+++ lib/pages/task_detail.page.dart
@@ -17,35 +17,47 @@ class TaskDetailPage extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          children: [
-            const TextField(
-              decoration: InputDecoration(
-                labelText: "Título",
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            children: [
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: "Título",
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const TextField(
-              decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Descrição",
+              const SizedBox(
+                height: 15,
               ),
-              maxLines: 5,
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text("Salvar Tarefa"),
-            )
-          ],
-        )
+              const TextField(
+                decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Descrição",
+                ),
+                maxLines: 5,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text("Salvar Tarefa"),
+              ),
+              Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Data de Criação: 12/05/2024"),
+                  IconButton(onPressed: () {}, 
+                  icon: const Icon(Icons.delete_outline),
+                  iconSize: 28,)
+                ],
+              )
+            ],
+          ),
         ),
-      );
+      ),
+    );
   }
 }
